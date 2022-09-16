9eed331f61a415d479ab86a5a99501e9b7050da4
Amy Gori <amydgori@gmail.com>
2022-09-16 13:07:20 -0400

    Limit queryset to objects that belong to the authenticated user

--- api/views.py
+++ api/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from .serializers import RecipeSerializer
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from core.models import Recipe
 
 class RecipeListCreateView(ListCreateAPIView):
-    queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
+    def get_queryset(self):
+        # write a query that gets recipes that were authored by the authenticated user
+        # You could do it like this:
+        # queryset = Recipe.objects.filter(author=self.request.user)
+        # But you can also use the related name from your Recipe model!
+        queryset = self.request.user.recipes.all()
+        return queryset
