4fc1092339f21c81a8f7f0213bf1a19048d5934b
Amy Gori <amydgori@gmail.com>
2022-09-17 16:48:29 -0400

    Add names for existing urls

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,10 @@ from rest_framework import routers
 from . import views
 
 urlpatterns = [
-    path("recipes/", views.RecipeListCreateView.as_view()),
-    path("recipes/<int:pk>/ingredients", views.IngredientCreateView.as_view()),
+    path("recipes/", views.RecipeListCreateView.as_view(), name="recipe-list"),
+    path(
+        "recipes/<int:pk>/ingredients",
+        views.IngredientCreateView.as_view(),
+        name="new-ingredient",
+    ),
 ]
